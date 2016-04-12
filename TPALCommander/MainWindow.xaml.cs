042b4ff7a9a2ca49143d9d12b7ef7f3d120d32b9
Patryk Kudła <patrykk.93@gmail.com>
2016-04-12 21:11:41 +0200

    Listviewbinding

--- TPALCommander/MainWindow.xaml.cs
+++ TPALCommander/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,69 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+public enum EntryType
+{
+    Dir,
+    File
+}
+
+public class DirectoryEntry
+{
+    public string Name { get; set; }
+
+    public string Extension { get; set; }
+
+    public string Size { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Imagepath { get; set; }
+
+    public EntryType Type { get; set; }
+
+    public string Fullpath { get; set; }
+}
+
+
+
+
+
 namespace TPALCommander
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+
+    
+
+    partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            ObservableCollection<DirectoryEntry> Collection = new ObservableCollection<DirectoryEntry>();
+            Collection.Add(new DirectoryEntry() {Name = "Xd", Date  = DateTime.Now, Extension = "exe", Fullpath = "C:/Fraps", Size = "1 000 kB", Type = EntryType.Dir, Imagepath = "s"});
+            //this.LeftView.ActualWidth = LeftGrid.ActualWidth;
+            LeftView.ItemsSource = Collection;
+        }
+
+        private void listViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void List_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ListView listView = sender as ListView;
+            GridView gView = listView.View as GridView;
+
+            var workingWidth = listView.ActualWidth - 10; // take into account vertical scrollbar
+            var col1 = 0.40;
+            var col2 = 0.20;
+
+            gView.Columns[0].Width = workingWidth * col1;
+            gView.Columns[1].Width = workingWidth * col2;
+            gView.Columns[2].Width = workingWidth - gView.Columns[1].Width - gView.Columns[0].Width;
         }
     }
 }
